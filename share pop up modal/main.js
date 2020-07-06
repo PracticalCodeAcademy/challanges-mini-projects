eb4ef45cd8ace32e44e1b61cf3b7b81df8c065ea
Practical Code Academy <67136508+PracticalCodeAcademy@users.noreply.github.com>
2020-07-06 17:45:38 -0400

    Add files via upload

---
+++ share pop up modal/main.js
@@ -0,0 +1,12 @@
+let closeBtn = document.querySelector('#close-btn');
+let card = document.querySelector('.card');
+let closeLink = document.querySelector('.close-link');
+
+
+closeBtn.addEventListener('click', ()=>{0
+    card.classList.add('close');
+})
+
+closeLink.addEventListener('click', ()=>{
+    card.classList.add('close');
+})
